83a257a9abd0eef7d27d2c8257e5ce6dba78fc23
mustafa-design <tifa20126@example.com>
2023-05-18 16:59:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NewComponent from './NewComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NewComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
